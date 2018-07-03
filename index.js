1336588d5146d5b65505ac138e42702edd9a6a29
Gerson Garrido <ggarridom@uni.pe>
2018-07-03 02:44:07 -0500

    adding comments

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-const express = require('express')
-const path = require('path')
-const port = process.env.PORT || 8000
-const app = express()
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+const app = express();
 
-// serve static assets normally
-app.use(express.static(__dirname + '/data'))
+// Esta parte nos permite servir archivos estáticos
+app.use(express.static(__dirname + '/data'));
 
-app.use(function (req, res, next) {
+// Configuramos los permisos para acceder a nuestro servidor
+app.use((req, res, next) => {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    // Página desde la cual podemos recibir nuestros datos
     res.setHeader('Access-Control-Allow-Origin', 'https://vulnerability-map.herokuapp.com');
-
-    // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
     res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Pass to next layer of middleware
     next();
 });
 
-app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'data', 'distritos.json'))
-})
+// Esta parte nos permite enviar nuestro archivo JSON como respuesta
+app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, 'data', 'distritos.json'))
+});
 
-app.listen(port)
-console.log("server started on port " + port);
+app.listen(port);
